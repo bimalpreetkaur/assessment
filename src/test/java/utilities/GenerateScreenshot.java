354821fda132f228cbdba96e9fd48fae1b4bb1b3
ravi maurya <ravimaurya@MacBook-Pro-von-ravi.local>
2022-08-18 19:20:09 +0200

    initial commit

---
+++ src/test/java/utilities/GenerateScreenshot.java
@@ -0,0 +1,23 @@
+package utilities;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+import java.io.File;
+import java.io.IOException;
+
+public class GenerateScreenshot extends BaseClass {
+    public GenerateScreenshot(String browserProperties) {
+        super(browserProperties);
+    }
+    public static long PAGE_LOAD_TIMEOUT = 10;
+    public static long IMPLICIT_WAIT = 10;
+
+    public static void takeScreenshotAtEndOfTest() throws IOException {
+        File scrFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+        String currentDir = System.getProperty("user.dir");
+        FileUtils.copyFile(scrFile, new File(currentDir + "/screenshots/" + System.currentTimeMillis() + ".png"));
+    }
+
+}
